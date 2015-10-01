e37e98a2c0d86693d9ba6a6ffb5f5c6b2e1c6733
Daan Boerlage <daan@boerlage.me>
2015-10-01 12:55:01 +0200

    init

---
+++ conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 20s
+killall conky
+cd "/home/daan/.conky/Green Apple Desktop"
+conky -c "/home/daan/.conky/Green Apple Desktop/Gotham" &
+cd "/home/daan/.conky/TeejeeTech"
+conky -c "/home/daan/.conky/TeejeeTech/Network Panel" &
+cd "/home/daan/.conky/TeejeeTech"
+conky -c "/home/daan/.conky/TeejeeTech/Process Panel" &
---
+++ readme.md
@@ -0,0 +1,5 @@
+# My Conkyrc
+
+I made this repo because some of you really wanted to see my .conkyrc
+
+I have not writen it myself, but made it with a tool called [Conky Manager](http://www.teejeetech.in/p/conky-manager.html), it's really cool and you should check it out if you want to do cool stuff with conky.
